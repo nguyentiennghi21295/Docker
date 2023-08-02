f166d65bc5a34cc48857fb8f7dd9f27cee3e170f
NGUYEN <nguyentiennghi212952019@gmail.com>
2023-08-02 12:51:31 +0200

    &é

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN yum install atk cups-libs gtk3 libXcomposite alsa-lib \
     libXtst pango at-spi2-atk libXt xorg-x11-server-Xvfb \
     xorg-x11-xauth dbus-glib dbus-glib-devel -y
 RUN pip install selenium==4.9.1 requests
-COPY --from=build /opt/chrome-linux /opt/chrome
-COPY --from=build /opt/chromedriver /opt/
+COPY --from=build /opt/chrome-linux/ /opt/chrome/
+COPY --from=build /opt/chromedriver/ /opt/
+COPY main.py ./
 CMD [ "main.handler" ]
--- main.py
+++ main.py
@@ -1,9 +1,44 @@
+import requests
+import json
+import string
+import random
+import os
 from selenium import webdriver
 from tempfile import mkdtemp
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def handler(event=None, context=None):
+CLIENT_ID = '78v7vw8a6gbdr4'
+CLIENT_SECRET = 'MxIQdlEGkRGGxlAe'
+REDIRECT_URL = 'http://localhost:8000/'
+
+
+letters = string.ascii_lowercase
+random.choice(letters)
+CSRF_TOKEN = ''.join(random.choice(letters) for i in range(24))
+
+
+# Your LinkedIn credentials
+USERNAME = 'nguyentiennghi212@gmail.com'
+PASSWORD = 'edogawaconan'
+
+auth_params1 = {
+    'response_type': 'code',
+    'client_id': CLIENT_ID,
+    'redirect_uri': REDIRECT_URL,
+    'state': CSRF_TOKEN,
+    'scope': 'r_liteprofile,r_emailaddress,w_member_social'
+}
+
+
+response = requests.get('https://www.linkedin.com/oauth/v2/authorization', params=auth_params1)
+login_url = response.url
+
+def get_authorization_code():
     options = webdriver.ChromeOptions()
     options.binary_location = '/opt/chrome/chrome'
     options.add_argument('--headless')
@@ -18,7 +53,44 @@ def handler(event=None, context=None):
     options.add_argument(f"--data-path={mkdtemp()}")
     options.add_argument(f"--disk-cache-dir={mkdtemp()}")
     options.add_argument("--remote-debugging-port=9222")
-    chrome = webdriver.Chrome("/opt/chromedriver",
+    driver_path = "/opt/chromedriver"
+    service = ChromeService(executable_path="/opt/chromedriver")
+    driver = webdriver.Chrome(service = service,
                               options=options)
-    chrome.get("https://example.com/")
-    return chrome.find_element(by=By.XPATH, value="//html").text
+    driver.get(login_url)
+    email1 = driver.find_element(By.ID, "username")
+    password1 = driver.find_element(By.ID, "password")
+    email1.send_keys(USERNAME)
+    password1.send_keys(PASSWORD)
+    driver.find_element(By.CSS_SELECTOR, '.btn__primary--large').click()
+
+        # Step 3: Wait for the redirect and extract the authorization code from the URL
+    WebDriverWait(driver, 10).until(
+        EC.url_contains(REDIRECT_URL) and EC.url_contains('code=')
+    )
+    AUTH_CODE = driver.current_url.split('code=')[1].split('&')[0]
+    driver.quit()    
+    return AUTH_CODE
+
+def lambda_handler(event=None, context=None):
+    AUTH_CODE = get_authorization_code()
+    ACCESS_TOKEN_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
+    access_data = {
+         'grant_type': 'authorization_code',
+         'code': AUTH_CODE,
+        'redirect_uri': REDIRECT_URL,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }
+    response = requests.post(ACCESS_TOKEN_URL, data=access_data, timeout=60)
+    response = response.json()
+    access_token = response['access_token']
+    params = {'oauth2_access_token': access_token}
+    response = requests.get('https://api.linkedin.com/v2/me', params=params)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response.json(), indent=1)
+    }
+
+lambda_handler()
